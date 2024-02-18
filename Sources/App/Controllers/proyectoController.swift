aace64b602227395ec8f61360a5ecc977884df28
ivan <ivan@example.com>
2024-02-18 19:53:52 +0100

    Cambio6

--- Sources/App/Controllers/proyectoController.swift
+++ Sources/App/Controllers/proyectoController.swift
@@ -5,8 +5,10 @@ struct ProyectoControlador: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let rutasProyectos = routes.grouped("proyectos")
 
+        rutasProyectos.delete(":id", use: deleteProyecto)
         rutasProyectos.get(use: obtenerTodos)
         rutasProyectos.post(use: crear)
+        rutasProyectos.put(":id", use: actualizarProyecto)
     }
     
     // Manejador para obtener todos los proyectos
@@ -18,5 +20,47 @@ struct ProyectoControlador: RouteCollection {
     func crear(_ req: Request) throws -> EventLoopFuture<PublicarProyecto> {
     let proyecto = try req.content.decode(PublicarProyecto.self)
     return proyecto.save(on: req.db).map { proyecto }
+
+    }
+    func deleteProyecto(req: Request) async throws -> HTTPStatus {
+
+    guard let proyectoID = req.parameters.get("id", as: Int.self) else {
+        throw Abort(.badRequest, reason: "ID del proyecto no proporcionado")
+    }    
+    guard let proyecto = try await PublicarProyecto.find(proyectoID, on: req.db) else {
+        throw Abort(.notFound, reason: "No existe el proyecto")
+    }
+    try await proyecto.delete(on: req.db)
+    return .ok
 }
+
+func actualizarProyecto(req: Request) async throws -> PublicarProyecto {
+    guard let proyectoID = req.parameters.get("id", as: Int.self) else {
+        throw Abort(.badRequest, reason: "ID del proyecto no proporcionado")
+    }
+
+    guard let proyectoExistente = try await PublicarProyecto.find(proyectoID, on: req.db) else {
+        throw Abort(.notFound, reason: "Proyecto no encontrado con el ID proporcionado")
+    }
+
+    let datosActualizados = try req.content.decode(PublicarProyecto.self)
+
+    proyectoExistente.nombreProyecto = datosActualizados.nombreProyecto ?? proyectoExistente.nombreProyecto
+    proyectoExistente.localizacion = datosActualizados.localizacion ?? proyectoExistente.localizacion
+    proyectoExistente.nombreCreador = datosActualizados.nombreCreador ?? proyectoExistente.nombreCreador
+    proyectoExistente.disposicion = datosActualizados.disposicion ?? proyectoExistente.disposicion
+    proyectoExistente.numeroInscritos = datosActualizados.numeroInscritos ?? proyectoExistente.numeroInscritos
+    proyectoExistente.estudios = datosActualizados.estudios ?? proyectoExistente.estudios
+    proyectoExistente.cursoRecomendado = datosActualizados.cursoRecomendado ?? proyectoExistente.cursoRecomendado
+    proyectoExistente.idiomas = datosActualizados.idiomas ?? proyectoExistente.idiomas
+    proyectoExistente.certificaciones = datosActualizados.certificaciones ?? proyectoExistente.certificaciones
+    proyectoExistente.descripcionProyecto = datosActualizados.descripcionProyecto ?? proyectoExistente.descripcionProyecto
+
+    try await proyectoExistente.save(on: req.db)
+
+    return proyectoExistente
+}
+
+
+    
 }
